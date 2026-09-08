4fcb6230b6b780a354f104ea5dc66e4d8f789258
narin6498 <63385948+narin6498@users.noreply.github.com>
2026-09-08 22:03:34 +0700

    V8.8.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='thaipain-v8.8.8-cleanhtml';
+const CACHE='thaipain-v8.8.9-singleview';
 const CORE=['./','./index.html','./app.html','./manifest.webmanifest','./human_front.png','./human_back.png','./human_left.png','./human_right.png','./icon-192.png','./icon-512.png','./offline.html'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
